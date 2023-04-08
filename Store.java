fe136f41a30a848c0d53e9e5a161c7ea02f155bd
araklid <122831751+araklid@users.noreply.github.com>
2023-04-08 11:59:05 -0400

    Create Store.java
    Allows for adding products and modifying products now.

---
+++ Store.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+//Sellers can view a list of their sales by store, including customer information and revenues from the sale.
+
+public class Store {
+    private ArrayList<Product> products;
+    private String name;
+    public Store(ArrayList<Product> products, String name) {
+        this.products = products;
+        this.name = name;
+    }
+    public Store(String name) {
+        this.name = name;
+        products = new ArrayList<>();
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void modifyProduct(Product product, String name, String nameOfStore, String description, double price, int quantityAvailable, Seller seller) {
+
+        for (int i = 0; i < products.size(); i++) {
+            if (product.equals(products.get(i))) {
+                Product newProduct = new Product(name, nameOfStore, description, quantityAvailable, price);
+                products.set(i, newProduct);
+            }
+        }
+
+    }
+
+
+
+}
